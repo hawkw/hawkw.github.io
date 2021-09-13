0574b1a9bfb61755ae785513ad2c5dd24861e6f2
Eliza Weisman <eliza@buoyant.io>
2021-09-13 13:04:14 -0700

    fixup nix-shell
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- shell.nix
+++ shell.nix
@@ -1,25 +1,24 @@
 scope@{ pkgs ? import <nixpkgs> { } }:
 
 pkgs.mkShell {
-  buildInputs = with pkgs;
-    [
-      git
-      bash
-      direnv
-      binutils
-      stdenv
-      bashInteractive
-      cacert
-      gcc
-      cmake
-      pkg-config
-      openssl
-      bundler
-      (glibcLocales.override { locales = [ "en_US.UTF-8" ]; })
-      remarshal
-      snappy
-      gnumake
-      autoconf
-      zlib
-    ] ++ stdenv.lib.optional stdenv.isDarwin [ Security libiconv ];
+  buildInputs = with pkgs; [
+    git
+    bash
+    direnv
+    binutils
+    stdenv
+    bashInteractive
+    cacert
+    gcc
+    cmake
+    pkg-config
+    openssl
+    bundler
+    (glibcLocales.override { locales = [ "en_US.UTF-8" ]; })
+    remarshal
+    snappy
+    gnumake
+    autoconf
+    zlib
+  ];
 }
